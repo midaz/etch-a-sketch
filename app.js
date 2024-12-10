bff805df40ae38709a8ff968f30a718368b6bf45
Dev Iyer <devpiyer@gmail.com>
2024-12-09 16:47:56 -0800

    Added user input button to set new grid size

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 let enterEventCount = 0; // Persist these counters globally
 let leaveEventCount = 0;
 
-function etchSketch(gridNumber = 16) {
+    
+// Handle button
+function getGridSize() {
+    const btn = document.querySelector("#btn");
+    btn.addEventListener("click", () => {
+        let gridSize= prompt("What is your grid size?");
+        gridSize = Number(gridSize); 
+        if (!isNaN(gridSize) && gridSize > 0 && gridSize <=100) { // Validate input
+            etchSketch(gridSize); // Pass gridSize to etchSketch
+        } else {
+            alert("Please enter a valid number between 1-100!");
+        }
+});
+}
+
+function etchSketch(gridSize=16) {
+    console.log(gridSize);
     // Create grid
     const container = document.querySelector(".container");
     container.innerHTML = ""; // Clear previous grid
 
-    for (let i = 0; i < gridNumber; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
 
-        for (let j = 0; j < gridNumber; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const column = document.createElement("div");
             column.classList.add("grid");
             row.appendChild(column);
@@ -30,7 +46,11 @@ function etchSketch(gridNumber = 16) {
             grid.style.backgroundColor = "white"; // Resets the style when the mouse leaves
         });
     });
+
 }
+
+// Get grid size from button
+getGridSize()
 
 // Initialize the grid
 etchSketch();
